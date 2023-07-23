25979231fca7e7d6390558dc06b226f2e8b660db
Natalia Doroshenko <Natalica.22@yandex.ru>
2023-07-23 19:08:03 +0700

    feat: верстка AppHeader

---
+++ src/components/app-header/app-header.jsx
@@ -0,0 +1,34 @@
+import {
+  Logo,
+  BurgerIcon,
+  ListIcon,
+  ProfileIcon,
+} from "@ya.praktikum/react-developer-burger-ui-components";
+import styles from './app-header.module.css';
+
+export default function AppHeader() {
+
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.navbar} pb-4 pt-4`}>
+        <div className={styles.container}>
+          <a className={styles.link}>
+            <BurgerIcon type="primary" />
+            <p className="text text_type_main-default pl-2">Конструктор</p>
+          </a>
+          <a className={styles.link}>
+            <ListIcon type="secondary" />
+            <p className="text text_type_main-default text_color_inactive pl-2">Лента заказов</p>
+          </a>
+        </div>
+        <div className={styles.logo}>
+          <Logo />
+        </div>
+        <a className={`${styles.link} ${styles.link_type_left}`}>
+          <ProfileIcon type="secondary" />
+          <p className="text text_type_main-default text_color_inactive pl-2">Личный кабинет</p>
+        </a>
+      </nav>
+    </header>
+  );
+}
