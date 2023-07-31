69ccb96678eb7559299703e153367daa9dc7ecf3
Natalia Doroshenko <Natalica.22@yandex.ru>
2023-08-01 00:07:49 +0700

    feat: свечение номера заказа

--- src/components/burger-constructor/burger-constructor.jsx
+++ src/components/burger-constructor/burger-constructor.jsx
@@ -69,7 +69,7 @@ export default function BurgerConstructor({ ingrediens }) {
       {
         modalVisible && 
         <Modal handleCloseClick={closeModal}>
-          <h2 className="text text_type_digits-large pt-4 pb-8">034536</h2>
+          <h2 className={`text text_type_digits-large pt-4 pb-8 ${styles.order_number}`}>034536</h2>
           <p className="text text_type_main-medium">идентификатор заказа</p>
           <img src={doneImage} alt="оформлено" className="pt-15 pb-15"/>
           <p className="text text_type_main-default pb-2">Ваш заказ начали готовить</p>
