2584ca08d56b1e2ac4200ed50357546d6000fd8b
Natalia Doroshenko <Natalica.22@yandex.ru>
2023-08-02 23:19:05 +0700

    fix: использование ссылок в header-link

--- src/components/app-header/header-link/header-link.jsx
+++ src/components/app-header/header-link/header-link.jsx
@@ -1,13 +1,12 @@
-import { Button } from "@ya.praktikum/react-developer-burger-ui-components";
 import styles from "./header-link.module.css";
 import PropTypes from "prop-types";
 
 export default function HeaderLink({ extraClass, children }) {
 
   return (
-    <Button htmlType="button" type="secondary" size="large" extraClass={`text text_type_main-default ${styles.link} pt-4 pb-4 pl-5 pr-5 ${extraClass}`}>
+    <a href="#" className={`text text_type_main-default ${styles.link} pt-4 pb-4 pl-5 pr-5 ${extraClass}`}>
       {children}
-    </Button>
+    </a>
   );
 }
 
