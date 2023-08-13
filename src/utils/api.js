c56a01368fea2f636dafb6b027ed4bfc3ca5685b
Natalia Doroshenko <Natalica.22@yandex.ru>
2023-08-13 23:35:01 +0700

    refactor: переименование аргумента

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,14 +1,14 @@
 import { API_URL } from "./constants";
 
-async function callApi(resource, method = 'GET', obj) {
+async function callApi(resource, method = 'GET', data) {
   const request = {
     method: method,
     headers: {
       'Content-Type': 'application/json'
     }
   }
-  if (obj) {
-    request.body = JSON.stringify(obj);
+  if (data) {
+    request.body = JSON.stringify(data);
   }
   const response = await fetch(API_URL + resource, request);
   return await (response.ok ? response.json() : Promise.reject(`Ошибка: ${response.status}`));
