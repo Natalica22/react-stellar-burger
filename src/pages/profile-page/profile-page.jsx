1f1254e10454d27723c0dd0ca4ca254d23e4894a
Natalia Doroshenko <Natalica.22@yandex.ru>
2023-10-08 21:03:28 +0700

    refactor: вынос меню профиля в отдельный компонент

--- src/pages/profile-page/profile-page.jsx
+++ src/pages/profile-page/profile-page.jsx
@@ -1,16 +1,11 @@
-import { Link, NavLink } from "react-router-dom";
 import styles from "./profile-page.module.css";
 import { Button, Input } from "@ya.praktikum/react-developer-burger-ui-components";
-import * as pages from "../../utils/pages"
-import { ACCESS_TOKEN, REFRESH_TOKEN, api } from "../../utils/api";
 import { useDispatch, useSelector } from "react-redux";
-import { patchUser, setUser } from "../../services/actions/user";
+import { patchUser } from "../../services/actions/user";
 import { useEffect, useRef, useState } from "react";
 import { useForm } from "../../hooks/useForm";
+import { ProfileMenu } from "../../components/profile-menu/profile-menu";
 
-function getLinkClass({ isActive }) {
-  return `${styles.link} text text_type_main-medium ` + (isActive ? styles.active_link : 'text_color_inactive');
-}
 
 export function ProfilePage() {
   const dispatch = useDispatch();
@@ -19,7 +14,7 @@ export function ProfilePage() {
 
   const user = useSelector(getUser);
 
-  const {form, onChange, setForm} = useForm({ ...user, password: '' });
+  const { form, onChange, setForm } = useForm({ ...user, password: '' });
   const [editActive, setEditActive] = useState(false);
 
   const nameInputRef = useRef(null);
@@ -41,44 +36,10 @@ export function ProfilePage() {
     setForm({ ...user, password: '' });
   }
 
-  const logout = () => {
-    api.logout()
-      .then(() => {
-        localStorage.removeItem(ACCESS_TOKEN);
-        localStorage.removeItem(REFRESH_TOKEN);
-
-        dispatch(setUser(null));
-      })
-      .catch(error => console.log(error));
-  }
-
   return (
     <main className={styles.main}>
       <div className={styles.container}>
-        <div className={`${styles.menu} pl-5`}>
-          <div className={styles.nav}>
-            <NavLink
-              end
-              to={pages.PROFILE_PAGE}
-              className={getLinkClass}>
-              Профиль
-            </NavLink>
-            <NavLink
-              end
-              to={pages.ORDERS_HISTORY_PAGE}
-              className={getLinkClass}>
-              История заказов
-            </NavLink>
-            <Link
-              className={`${styles.link} text text_type_main-medium text_color_inactive`}
-              onClick={logout}>
-              Выход
-            </Link>
-          </div>
-          <p className={`text text_type_main-default ${styles.description}`}>
-            В этом разделе вы можете изменить&nbsp;свои&nbsp;персональные&nbsp;данные
-          </p>
-        </div>
+        <ProfileMenu />
         <div className={styles.profile}>
           <form className={styles.form} onSubmit={submitForm} onReset={resetForm}>
             <Input
