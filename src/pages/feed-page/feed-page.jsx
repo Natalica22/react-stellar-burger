8e1a48e25bafd5b38f9dbdd4e9b05aedc5d07d72
Natalia Doroshenko <Natalica.22@yandex.ru>
2023-10-11 01:02:31 +0700

    feat: чтение данных с помощью ws для feed-page

--- src/pages/feed-page/feed-page.jsx
+++ src/pages/feed-page/feed-page.jsx
@@ -1,10 +1,24 @@
 import { OrdersList } from "../../components/orders-list/orders-list";
 import styles from "./feed-page.module.css";
-import { orders } from "../../utils/constants";
+import { FEED_WS_URL } from "../../utils/constants";
 import { OrdersStatus } from "../../components/orders-status/orders-status";
 import { FEED_PAGE } from "../../utils/pages";
+import { useDispatch, useSelector } from "react-redux";
+import { useEffect } from "react";
+import { FEED_WS_CLOSE, FEED_WS_CONNECT } from "../../services/actions/wsFeedOrders";
 
 export function FeedPage() {
+  const dispatch = useDispatch();
+
+  const getOrdersData = store => store.wsFeedOrders.data;
+  const ordersData = useSelector(getOrdersData);
+
+  const orders = ordersData ? ordersData.orders : [];
+
+  useEffect(() => {
+    dispatch({ type: FEED_WS_CONNECT, payload: FEED_WS_URL });
+    return () => dispatch({ type: FEED_WS_CLOSE });
+  }, [dispatch]);
 
   return (
     <main className={styles.main}>
